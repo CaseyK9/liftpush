43ea790e289e1745eca879c6a21de8cace7a01d5
James <jselby@jselby.net>
2017-12-19 00:54:57 +1100

    Add differing endpoints for uploading

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,9 @@ mod config;
 
 use rocket::Data;
 use rocket::request::Form;
-use rocket::response::{NamedFile, Content, Stream, Redirect};
+use rocket::request::Request;
+use rocket::response;
+use rocket::response::{NamedFile, Content, Stream, Redirect, Responder};
 use rocket::http::ContentType;
 use rocket::http::Cookie;
 use rocket::http::Cookies;
@@ -60,11 +62,23 @@ struct UploadStatus {
     url : String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 enum FileType {
     File,
-    Url
+    Url,
+    Text
+}
+
+impl FileType {
+    fn from_str(name : &str) -> Option<FileType> {
+        match name {
+            "file" => Some(FileType::File),
+            "url" => Some(FileType::Url),
+            "text" => Some(FileType::Text),
+            _ => None
+        }
+    }
 }
 
 #[allow(dead_code)]
@@ -93,17 +107,39 @@ struct FileMetadata {
     date: DateTime<FixedOffset>, // Mon, 11 Dec 2017 10:28:36 +0000"
     #[serde(rename = "type")]
     file_type : FileType,
-    filename : String,
-    actual_filename : String
+    url : Option<String>,
+    filename : Option<String>,
+    actual_filename : Option<String>
 }
 
 impl FileMetadata {
-    fn new(file_type : FileType, filename : String, actual_filename : String) -> Self {
+    fn new_from_file(filename : String, actual_filename : String) -> Self {
         Self {
             date : Local::now().with_timezone(&FixedOffset::east(0)),
-            file_type,
-            filename,
-            actual_filename
+            file_type : FileType::File,
+            filename : Some(filename),
+            actual_filename : Some(actual_filename),
+            url : None
+        }
+    }
+
+    fn new_from_text(filename : String, actual_filename : String) -> Self {
+        Self {
+            date : Local::now().with_timezone(&FixedOffset::east(0)),
+            file_type : FileType::Text,
+            filename : Some(filename),
+            actual_filename : Some(actual_filename),
+            url : None
+        }
+    }
+
+    fn new_from_url(url : String) -> Self {
+        Self {
+            date : Local::now().with_timezone(&FixedOffset::east(0)),
+            file_type : FileType::Url,
+            filename : None,
+            actual_filename : None,
+            url : Some(url)
         }
     }
 }
@@ -138,10 +174,17 @@ fn parse_meta(file : &str) -> Option<FileMetadata> {
     }
 }
 
-#[post("/", data = "<data>", rank = 1)]
+#[post("/<input_type>", data = "<data>", rank = 1)]
 fn upload(data : Data, _user : APIUser, boundary : MultipartBoundary, out_file : RandomFilename,
-    config : State<Config>)
+    config : State<Config>, input_type : String)
     -> Result<Json<UploadStatus>, String> {
+    let input_type = match FileType::from_str(&input_type) {
+        Some(val) => val,
+        _ => return Err(format!("Invalid input type"))
+    };
+
+    println!("Uploading: {:?}", input_type);
+
     // Rocket does not support multipart forms (for whatever goddamn reason),
     //  so we directly hook 'multipart' here.
     let mut mp = Multipart::with_body(data.open(), boundary.boundary);
@@ -174,8 +217,57 @@ fn upload(data : Data, _user : APIUser, boundary : MultipartBoundary, out_file :
             };
 
             // Generate metadata
-            let meta = FileMetadata::new(FileType::File, original_filename,
-                              new_filename.clone());
+            // TODO: Unwrap
+            let meta = match input_type {
+                FileType::File => {
+                    println!("Save file to {}", new_filename);
+                    // TODO: Check to make sure file doesn't exist
+                    file.save().with_path("d/".to_string() + &new_filename)
+                        .into_result_strict().unwrap();
+                    FileMetadata::new_from_file(original_filename,
+                                                new_filename.clone())
+                },
+                FileType::Text => {
+                    println!("Save file to {}", new_filename);
+
+                    let mut data = String::new();
+                    file.read_to_string(&mut data).unwrap();
+
+                    if data[0 .. 8].contains("://") {
+                        FileMetadata::new_from_url(data)
+                    } else {
+                        // Save buffered text
+                        let meta_filename = "d/".to_string() + &new_filename;
+                        let path = Path::new(&meta_filename);
+
+                        let mut meta_file = match File::create(&path) {
+                            Err(why) => {
+                                println!("Couldn't create {}: {}",
+                                         meta_filename,
+                                         why.description());
+                                return Err(format!("Failed to write file"));
+                            },
+                            Ok(file) => file,
+                        };
+
+                        match meta_file.write_all(data.as_bytes()) {
+                            Err(why) => {
+                                println!("Failed to write to {}: {}", meta_filename,
+                                         why.description());
+                                return Err(format!("Failed to write file"));
+                            },
+                            Ok(_) => println!("successfully wrote to {}", meta_filename),
+                        }
+
+                        FileMetadata::new_from_text(original_filename,
+                                                    new_filename.clone())
+                    }
+                },
+                _ => { // URL
+                    return Err(format!("Type not supported!"));
+                }
+            };
+
             let meta_string = match serde_json::to_string(&meta) {
                 Ok(data) => data,
                 Err(msg) => {
@@ -208,12 +300,6 @@ fn upload(data : Data, _user : APIUser, boundary : MultipartBoundary, out_file :
                 Ok(_) => println!("successfully wrote to {}", meta_filename),
             }
 
-            // TODO: Unwrap
-            println!("Save file to {}", new_filename);
-            // Check to make sure file doesn't exist
-            file.save().with_path("d/".to_string() + &new_filename)
-                .into_result_strict().unwrap();
-
             Ok(Json(UploadStatus {
                 url: config.base_url.clone() + &url
             }))
@@ -222,8 +308,8 @@ fn upload(data : Data, _user : APIUser, boundary : MultipartBoundary, out_file :
     }
 }
 
-#[post("/", data = "<_data>", rank = 2)]
-fn upload_no_auth(_data : Data) -> Result<String, String> {
+#[post("/<_url_type>", data = "<_data>", rank = 2)]
+fn upload_no_auth(_url_type : String, _data : Data) -> Result<String, String> {
     Err(format!("Non-authed request"))
 }
 
@@ -255,10 +341,10 @@ fn login(task: Form<LoginAttempt>, mut cookies: Cookies, sessions: State<Mutex<S
     if found {
         let session = sessions.lock().unwrap().new_session(username);
         cookies.add_private(Cookie::new(LOGIN_COOKIE, session));
+        Redirect::to(".")
+    } else {
+        Redirect::to(".?error=invalid-login")
     }
-
-    // TODO: Error messages
-    Redirect::to(".")
 }
 
 #[get("/logout")]
@@ -300,7 +386,7 @@ fn manage(user : User) -> Template {
     found_files.sort_by(|a, b|
         b.date.partial_cmp(&a.date).unwrap());
 
-    Template::render("test", &FileViewerState {
+    Template::render("manage", &FileViewerState {
         username : user.username,
         files : found_files
     })
@@ -308,7 +394,7 @@ fn manage(user : User) -> Template {
 
 #[get("/", rank = 2)]
 fn homepage() -> Template {
-    Template::render("hello", &{})
+    Template::render("index", &{})
 }
 
 #[get("/<file>")]
@@ -319,21 +405,87 @@ fn delete_file(_user : User, file: String) -> Option<String> {
 
     let meta = parse_meta(&file)?;
 
-    fs::remove_file(Path::new("d/").join(meta.actual_filename)).unwrap();
+    match meta.actual_filename {
+        Some(name) => fs::remove_file(Path::new("d/").join(name)).unwrap(),
+        _ => ()
+    }
+
     fs::remove_file(Path::new("d/").join(file + ".info.json")).unwrap();
 
     Some(format!("Deleted"))
 }
 
+enum FileResponseType {
+    File { file : NamedFile },
+    Redir { redir : Redirect },
+    Template { template : Template }
+}
+
+impl<'r> Responder<'r> for FileResponseType {
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        match self {
+            FileResponseType::File { file } => file.respond_to(req),
+            FileResponseType::Redir { redir } => redir.respond_to(req),
+            FileResponseType::Template { template } => template.respond_to(req)
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct TextView {
+    contents : String,
+    meta : FileMetadata
+}
+
 #[get("/<file>", rank = 3)]
-fn get_pushed_file(file: String) -> Option<NamedFile> {
+fn get_pushed_file(file: String) -> Option<FileResponseType> {
     if file.contains(".") || file.contains("/") || file.contains("\\") {
         return None;
     }
 
     let meta = parse_meta(&file)?;
 
-    NamedFile::open(Path::new("d/").join(meta.actual_filename)).ok()
+    match meta.file_type {
+        FileType::File => {
+            match NamedFile::open(Path::new("d/").join(meta.actual_filename.unwrap())).ok() {
+                Some(file) => Some(FileResponseType::File {
+                    file
+                }),
+                _ => None
+            }
+        },
+        FileType::Url => Some(FileResponseType::Redir {
+            redir : Redirect::temporary(&meta.url.unwrap())
+        }),
+        FileType::Text => {
+            // Read in text file
+            let meta_filename = "d/".to_string() + &meta.actual_filename.clone().unwrap();
+            let path = Path::new(&meta_filename);
+            let mut meta_file = match File::open(&path) {
+                Err(_) => {
+                    println!("File {} doesn't exist!", file);
+                    return None;
+                },
+                Ok(file) => file,
+            };
+
+            let mut meta_string = String::new();
+            match meta_file.read_to_string(&mut meta_string) {
+                Err(_) => {
+                    println!("File {} is unreadable!", file);
+                    return None;
+                },
+                Ok(_) => println!("successfully read {}", meta_filename),
+            }
+
+            Some(FileResponseType::Template {
+                template : Template::render("text", &TextView {
+                    contents : meta_string,
+                    meta
+                })
+            })
+        },
+    }
 }
 
 #[get("/<file..>", rank = 4)]
